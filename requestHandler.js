a2ca15f97aff2ce7740b04d6e3e02c806f8a4ef5
Robin Kim <therobinkim@gmail.com>
2015-08-05 22:22:13 -0700

    Add timestamp to setTimeout in server.js

--- requestHandler.js
+++ requestHandler.js
@@ -7,8 +7,11 @@ module.exports = function requestHandler(request, response) {
   request.on('end', function requestOnEnd() {
     response.write('data: ' + data + '\n');
     response.write('request.url: ' + request.url + '\n');
+    response.write('time: ' + new Date() + '\n');
     setTimeout(function respondEnd() {
-      response.end('1 second later: goodbye world');
+      response.write('one second later...\n');
+      response.write('time: ' + new Date() + '\n');
+      response.end('goodbye world');
     }, 1000);
   });
 };
