6120e2e2b21219ac8e3546e1c847ef0223a4cc79
Robin Kim <therobinkim@gmail.com>
2015-08-06 18:32:19 -0700

    Add more console.logs for response.on() events
    Adds more details for request.on('data') and request.on('end').

--- requestHandler.js
+++ requestHandler.js
@@ -11,10 +11,11 @@ module.exports = function requestHandler(request, response) {
   var data = '';
   response.log('this is a', request.method, 'request');
   request.on('data', function requestOnData(chunk) {
-    console.log('chunk');
+    console.log('chunk length:', chunk.length, 'bytes, or', chunk.length/1024, 'KiB');
     data += chunk;
   });
   request.on('end', function requestOnEnd() {
+    console.log('end');
     response.log('data:', data);
     response.log('request.url:', request.url);
     response.log('time:', new Date());
